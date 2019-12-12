af36d506e59d983944b5746aabca15b1cfee0c71
kevinmay <kevinmay_wang@hotmail.com>
2019-12-12 22:23:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World! Kevin Wang");
-    response.end("Hello Azure!");
+    response.end("Hello World! Hi Azure!");
 });
 
 const port = process.env.PORT || 1337;
